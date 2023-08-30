4a7cf89360d69e717087d12fa0b2a5b01a022328
reynaldosena <reynaldosenadev@gmail.com>
2023-08-29 22:29:54 -0300

    Ligando pros candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,59 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     
     public static void main(String[] args){
-        seleçaoCandidatos();
+        String candidatos[] = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
 
+        for (String candidato: candidatos){
 
+            entrandoEmContato(candidato);
 
+        }
+
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1 ;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+
+            else
+            System.out.println("Contato realizado com Sucesso");
+
+        }while (continuarTentando && tentativasRealizadas < 3);
+    
+        if(atendeu)
+        System.out.println("Conseguimos contato com " + candidato  + " na " + tentativasRealizadas + " tentativa");
+
+        else
+        System.out.println("Nao conseguimos contato com " + candidato + ", numero maximo de tentativas " + tentativasRealizadas + "ª realizada");
+
+    }
+
+        //método auxiliar
+        static boolean atender(){
+            return new Random().nextInt(3)==1;
+        }
+
+    static void imprimirSelecionados(){
+        String candidatos[] = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0; indice<candidatos.length; indice++){
+            System.out.println("o candidato de numero " + (indice+1) + " é "+ candidatos[indice]);
+        }
+
+        for(String candidato: candidatos){
+            System.out.println("o candidato selecionado foi " + candidato);
+        }
 
     }
 
